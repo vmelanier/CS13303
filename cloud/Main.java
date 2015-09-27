47cdf1836a24a1c53a80792b4db7daf022247649
JM Lopez <jm.lopez@utoronto.ca>
2015-09-26 21:05:51 -0400

    CS13303T05-5.2.2

--- cloud/Main.java
+++ cloud/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		NetworkCard nic1 = new NetworkCard();
 		nic1.network = "192.168.1.0";
 		nic1.macAddress = "64-D9-2C-A1-BD-B5";
+		nic1.speedMb = 100;
 		nic1.setUnit(1);
 
 		// Imprimiendo variables de instancia
@@ -16,6 +17,7 @@ public class Main {
 		NetworkCard nic2 = new NetworkCard();
 		nic2.network = "192.168.1.0";
 		nic2.macAddress = "5D-E1-5E-B1-9A-DB";
+		nic2.speedMb = 1000;     // 1000 Mb Full duplex
 		nic2.setUnit(2);
 
 		// Imprimiendo variables de instancia		
@@ -24,6 +26,7 @@ public class Main {
 	}
 	public static void printNICInfo(NetworkCard nic){
 		System.out.println("MAC Address: " + nic.macAddress);
+		System.out.println("Speed      : " + nic.speedMb +" " + (nic.speedMb <= NetworkCard.maxSpeedMb? "OK": "ERR"));
 		System.out.println("Network    : " + nic.network);
 		System.out.println("Label      : " + nic.label);
 		System.out.println("");
--- cloud/NetworkCard.java
+++ cloud/NetworkCard.java
@@ -1,6 +1,8 @@
 package cloud;
 
 class NetworkCard {
+	static int maxSpeedMb = 100;        // 100 Mb - Full Duplex
+	int speedMb;                        // Velocidad
 	String macAddress;                  // Dirección física
 	String network;                     // Segmento de red
 	int unitNumber = 0;                 // Número de unidad
@@ -10,6 +12,4 @@ class NetworkCard {
 		this.unitNumber = unit;
 		this.label = this.label + unit;
 	}
-	
-	
 }
