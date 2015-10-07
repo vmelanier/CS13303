296249205d9144e761f13a2b187fafe6107729e6
JM Lopez <jm.lopez@utoronto.ca>
2015-10-07 19:27:01 -0400

    CS13303T07-7.3.2

---
+++ errores/TryCatch.java
@@ -0,0 +1,31 @@
+package errores;
+
+public class TryCatch {
+	public static void main(String[] args) {
+		int[] array1 = {1, 2, 3, 4, 5, 3, 7};
+		int[] array2 = {5, 0, 3, 2, 1, 0};
+		
+		for (int i=0; i<array1.length; i++){
+			 try{
+				 System.out.println(String.format("%s/%s=%s", 
+						 array1[i], array2[i], array1[i]/array2[i]));
+				 
+				 // Creando un error NullPointerException
+				 if (array1[i] == 5){
+					 String nulo = null;
+					 System.out.println(nulo.length());
+				 }
+			 } catch (NullPointerException ex ){
+				 System.out.println(String.format("Occurrio un error de tipo %s: %s %s", 
+						 ex.toString(), ex.getMessage(), ex.getStackTrace()));				 
+			 } catch (ArithmeticException ex){
+				 System.out.println(String.format("Occurrio un error de tipo %s: %s %s", 
+						 ex.toString(), ex.getMessage(), ex.getStackTrace()));
+			 } catch (ArrayIndexOutOfBoundsException ex){
+				 System.out.println(String.format("Occurrio un error de tipo %s: %s %s", 
+						 ex.toString(), ex.getMessage(), ex.getStackTrace()));
+			 }
+		}
+		System.out.println("Fin del programa");
+	}
+}
