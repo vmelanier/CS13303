a6ad01b824c61a352dcfa7ad6587a1eea32060d8
JM Lopez <jm.lopez@utoronto.ca>
2015-10-07 20:22:58 -0400

    CS13303T07-7.3.4

--- errores/TryCatch.java
+++ errores/TryCatch.java
@@ -9,15 +9,16 @@ public class TryCatch {
 			 try{
 				 System.out.println(String.format("%s/%s=%s", 
 						 array1[i], array2[i], array1[i]/array2[i]));
-				 
-				 // Creando un error NullPointerException
-				 if (array1[i] == 5){
-					 String nulo = null;
-					 System.out.println(nulo.length());
-				 }
-			 } catch (NullPointerException ex ){        // Subclase
-				 System.out.println(String.format("Occurrio un error de tipo %s: %s %s", 
-						 ex.toString(), ex.getMessage(), ex.getStackTrace()));		
+				 try{
+					 // Creando un error NullPointerException
+					 if (array1[i] == 5){
+						 String nulo = null;
+						 System.out.println(nulo.length());
+						 }	
+					 } catch (NullPointerException ex ) {        // Subclase
+						 System.out.println(String.format("Occurrio un error de tipo %s: %s %s", 
+								 ex.toString(), ex.getMessage(), ex.getStackTrace()));
+						 }	
 			 } catch (Throwable ex){                    // Super clase
 				 System.out.println(String.format("Occurrio un error de tipo Throwable %s: %s %s", 
 						 ex.toString(), ex.getMessage(), ex.getStackTrace()));
