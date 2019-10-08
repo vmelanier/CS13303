e5b891692a0ddc62436e4aedc4363508e472a405
vmelanier <vmelanier@gmail.com>
2019-10-07 20:38:19 -0500

    Ajuste codigo HardDiskDrive

--- cloud/HardDiskDrive.java
+++ cloud/HardDiskDrive.java
@@ -6,6 +6,9 @@ public class HardDiskDrive {
 	int unitNumber;                 // Número de unidad
 	String label;	                // Etiqueta
 	
+	private static final int MIN_CAPACITY = 1;
+	private static final int CAPACITY_MULTIPLE = 40;
+
 	/* 
 	 * HardDiskDrive[1]
 	 * Crear constructor y validar lo siguiente:
@@ -17,7 +20,17 @@ public class HardDiskDrive {
 	public HardDiskDrive(long capacityGB, int unitNumber){
 		this.unitNumber = unitNumber;
 		// Escribe tu código {
-		
+		if ( capacityGB < MIN_CAPACITY) {
+			this.capacityGB = MIN_CAPACITY;
+			System.err.println("La capacidad minima es " + MIN_CAPACITY);
+		}
+
+		if ( capacityGB % CAPACITY_MULTIPLE !=0 ){
+			System.err.println("La capacidad debe ser multiplo de") + CAPACITY_MULTIPLE;
+
+		}
+		StringBuilder sb = new StringBuilder();
+		this.label = sb.append("Hard Disk").append(unitNumber).toString();
 		// }
 	}
 	
@@ -32,7 +45,9 @@ public class HardDiskDrive {
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
 		// Escribe tu código {
-
+	    sb.append("Etiqueta :").append(this.label).append("\n");
+        sb.append("Unidad   : ").append(this.unitNumber).append("\n");
+        sb.append("Capacidad: ").append(this.capacityGB).append("\n");
 		// }
 		return sb.toString();
 	}
